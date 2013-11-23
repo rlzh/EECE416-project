274c037ce29ba066a9ed71b50143d517c7e1b1b4
Weing Kai Wee <wngkai.91@gmail.com>
2013-11-23 15:44:52 -0800

    updated BaseTest and AccountTest

--- src/com/project/selenium/BaseTest.java
+++ src/com/project/selenium/BaseTest.java
@@ -2,22 +2,44 @@ package com.project.selenium;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 	
-	protected WebDriver driver;
+	protected static WebDriver driver;
+	protected static final String url = "http://www.instapaper.com/";
+	protected static final String username = "dummy@gmail.com";
+	protected static final String password = "123456";
 	
 	@BeforeClass
-	public void setUp() {
+	public static void setUp() {
 		driver = new FirefoxDriver();
+		driver.get(url);
 	}
 	
 	@AfterClass
-	public void closeFirefox() {
+	public static void closeFirefox() {
 		driver.close();
 	}
 	
+	public void logIn(String username, String password) {
+		
+	}
+	
+	public void createAccount(String username, String password) {
+		WebElement signIn = driver.findElement(By.linkText("Sign In"));
+		signIn.click();
+		
+		WebElement newUsername = driver.findElement(By.id("username"));
+		newUsername.sendKeys(username);
+		WebElement newPassword = driver.findElement(By.id("password"));
+		newPassword.sendKeys(password);
+		
+		WebElement register = driver.findElement(By.id("register"));
+		register.click();
+	}
 
 }
