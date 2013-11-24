2a8697b8bed4c9b7b0bb4252cfb5a86a614a0f51
Ryan Liu <ryan_lzh@hotmail.com>
2013-11-23 17:43:27 -0800

    Migrated code from unused test class & deleted it

--- src/org/project/test/ReadArticleTest.java
+++ src/org/project/test/ReadArticleTest.java
@@ -1,5 +1,49 @@
 package org.project.test;
 
-public class ReadArticleTest {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.project.selenium.BaseTest;
+
+public class ReadArticleTest extends BaseTest {
+
+	@Before
+	public void SetUp() {
+		super.logIn();
+
+	}
+	
+	@Test
+	public void SimpleReadTest() {
+		WebElement UnreadNavBtn = driver.findElement( By.id( "unread_nav" ) );
+		UnreadNavBtn.click();
+		
+		String articleTitle = "What the Xbox One tells us about 'the new Microsoft'";
+		WebElement articleLink = driver.findElement( By.linkText( articleTitle ) );
+		String articleUrl = articleLink.getAttribute("href");
+		
+		articleLink.click();
+		
+		assertTrue( driver.getCurrentUrl().equals( articleUrl ) );
+		CheckPageLayout();
+	}
+	
+	
+	private void CheckPageLayout() {
+		assertNotNull( driver.findElement( By.id("titlebar") ) );
+		assertNotNull( driver.findElement( By.id("story") ) );
+		assertNotNull( driver.findElement( By.id("footer") ) );
+		assertNotNull( driver.findElement( By.className("icon-globe") ) );
+		assertNotNull( driver.findElement( By.className("menuContainer") ) );
+		assertNotNull( driver.findElement( By.className("icon-like") ) );
+		assertNotNull( driver.findElement( By.className("deleteButton") ) );
+		assertNotNull( driver.findElement( By.className("archiveButton") ) );
+		assertNotNull( driver.findElement( By.className("shareOptions") ) );
+	}
+	
 }
--- src/org/project/test/UserOrganizeTest.java
+++ src/org/project/test/UserOrganizeTest.java
@@ -1,5 +1,29 @@
 package org.project.test;
 
-public class UserOrganizeTest {
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.project.selenium.BaseTest;
+
+public class UserOrganizeTest extends BaseTest{
+	@Before
+	public void SetUp() {
+		super.logIn();
+	}
+	
+	@Test
+	public void AddFolderTest() {
+		WebElement addFolderBtn = driver.findElement( By.id( "add_folder" ) );
+		String addFolderUrl = addFolderBtn.getAttribute("href");
+
+		addFolderBtn.click();
+		
+		assertTrue( driver.getCurrentUrl().equals(addFolderUrl) );
+		WebElement inputBox = driver.findElement( By.id( "foldertitle" ) );
+	}
+	
 }
