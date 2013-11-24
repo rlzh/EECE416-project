06e55873d12601404acf09f053829f35f0ee4d80
Ryan Liu <ryan_lzh@hotmail.com>
2013-11-24 13:12:43 -0800

    changed pom

--- src/org/project/test/BrowseArticleTest.java
+++ src/org/project/test/BrowseArticleTest.java
@@ -22,37 +22,51 @@ public class BrowseArticleTest extends BaseTest {
 	
 	@Test
 	public void readArticleNowTest() {
-		List<WebElement> stories = driver.findElements( By.cssSelector( ".headline" ) );
-		WebElement story = stories.get(0);		
-		WebElement storyLink = story.findElement( By.xpath( "./a" ) );
-		String storyUrl = storyLink.getAttribute("href");
-		
-		storyLink.click();
+		try {
+			List<WebElement> stories = driver.findElements( By.cssSelector( ".headline" ) );
+			WebElement story = stories.get(0);		
+			WebElement storyLink = story.findElement( By.xpath( "./a" ) );
+			String storyUrl = storyLink.getAttribute("href");
+			
+			storyLink.click();
 
-		assertTrue( driver.getCurrentUrl().equals(storyUrl) );
+			assertTrue( driver.getCurrentUrl().equals(storyUrl) );
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			fail();
+		}
+		
 	}
 	
 	@Test
 	public void readArticleLaterTest() {
-		List<WebElement> stories = driver.findElements( By.cssSelector( ".headline" ) );
-		WebElement story = stories.get(0);		
-		WebElement storyLink = story.findElement( By.xpath( "./a" ) );
-		String storyTitle = storyLink.getAttribute("title");
+		try {
+			List<WebElement> stories = driver.findElements( By.cssSelector( ".headline" ) );
+			WebElement story = stories.get(0);		
+			WebElement storyLink = story.findElement( By.xpath( "./a" ) );
+			String storyTitle = storyLink.getAttribute("title");
+			
+			List<WebElement> links = driver.findElements( By.className( "bookmarklet" ) );
+			WebElement readLaterLink = links.get(0);
+			
+			readLaterLink.click();
+			
+			driver.findElement( By.id( "unread_nav" ) ).click();
+			
+			WebElement article = driver.findElement( By.xpath( "//a[@title='"+ storyTitle + "']" ) );
+			assertNotNull( article );
+			
+			String articleId = article.getAttribute("href").substring(31);
+			System.out.println(articleId);
+			
+			super.removePage(articleId, storyTitle);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			fail();
+		}
 		
-		List<WebElement> links = driver.findElements( By.className( "bookmarklet" ) );
-		WebElement readLaterLink = links.get(0);
-		
-		readLaterLink.click();
-		
-		driver.findElement( By.id( "unread_nav" ) ).click();
-		
-		WebElement article = driver.findElement( By.xpath( "//a[@title='"+ storyTitle + "']" ) );
-		assertNotNull( article );
-		
-		String articleId = article.getAttribute("href").substring(31);
-		System.out.println(articleId);
-		
-		super.removePage(articleId, storyTitle);
 	}
 
 }
