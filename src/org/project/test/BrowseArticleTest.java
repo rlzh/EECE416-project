cfcd99ec043cd2c756799f145ed5764973ad7957
Ryan Liu <ryan_lzh@hotmail.com>
2013-11-23 22:16:51 -0800

    Implemented BrowseArticle tests

--- src/org/project/test/BrowseArticleTest.java
+++ src/org/project/test/BrowseArticleTest.java
@@ -1,5 +1,58 @@
 package org.project.test;
 
-public class BrowseArticleTest {
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import com.project.selenium.BaseTest;
+
+public class BrowseArticleTest extends BaseTest {
+	
+	@Before
+	public void setUp() {
+		super.setUp();
+		logIn();
+		WebElement browseNavBtn = driver.findElement( By.id( "browse_nav" ) );
+		browseNavBtn.click();
+	}
+	
+	@Test
+	public void readArticleNowTest() {
+		List<WebElement> stories = driver.findElements( By.cssSelector( ".headline" ) );
+		WebElement story = stories.get(0);		
+		WebElement storyLink = story.findElement( By.xpath( "./a" ) );
+		String storyUrl = storyLink.getAttribute("href");
+		
+		storyLink.click();
+
+		assertTrue( driver.getCurrentUrl().equals(storyUrl) );
+	}
+	
+	@Test
+	public void readArticleLaterTest() {
+		List<WebElement> stories = driver.findElements( By.cssSelector( ".headline" ) );
+		WebElement story = stories.get(0);		
+		WebElement storyLink = story.findElement( By.xpath( "./a" ) );
+		String storyTitle = storyLink.getAttribute("title");
+		
+		List<WebElement> links = driver.findElements( By.className( "bookmarklet" ) );
+		WebElement readLaterLink = links.get(0);
+		
+		readLaterLink.click();
+		
+		driver.findElement( By.id( "unread_nav" ) ).click();
+		
+		WebElement article = driver.findElement( By.xpath( "//a[@title='"+ storyTitle + "']" ) );
+		assertNotNull( article );
+		
+		String articleId = article.getAttribute("href").substring(31);
+		System.out.println(articleId);
+		
+		super.removePage(articleId, storyTitle);
+	}
 
 }
