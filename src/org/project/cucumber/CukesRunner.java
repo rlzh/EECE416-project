e7de1f220ff1eef10278f07e550e853cc22319f3
Weing Kai Wee <wngkai.91@gmail.com>
2013-11-24 12:24:40 -0800

    Added cucumber, again

---
+++ src/org/project/cucumber/CukesRunner.java
@@ -0,0 +1,12 @@
+package org.project.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+		)
+public class CukesRunner {
+
+}
