40a53226b7e1620ffe09c4dc549a0c348542e657
Ryan Liu <ryan_lzh@hotmail.com>
2013-11-23 16:24:30 -0800

    Added page layout check function to BaseTest

--- src/com/project/selenium/BaseTest.java
+++ src/com/project/selenium/BaseTest.java
@@ -2,6 +2,7 @@ package com.project.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import static org.junit.Assert.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -41,5 +42,36 @@ public class BaseTest {
 	public void createAccount(String username, String password) {
 		
 	}
-
+	
+	public void PageLayout() {
+		// check for site header
+		assertNotNull( driver.findElement( By.id( "header" ) ) );
+		// check for navigation side bar
+		assertNotNull( driver.findElement( By.id( "side_column" ) ) );
+		// check for buttons in navigation side bar
+		assertNotNull( driver.findElement( By.id( "user_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "like_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "arhcive_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "search_box" ) ) );
+		assertNotNull( driver.findElement( By.id( "browse_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "save_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "user_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "like_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "like_nav" ) ) );
+		assertNotNull( driver.findElement( By.id( "like_nav" ) ) );
+		// check for foot in navigation side bar
+		assertNotNull( driver.findElement( By.linkText( "Help" ) ) );
+		assertNotNull( driver.findElement( By.linkText( "Apps" ) ) );
+		assertNotNull( driver.findElement( By.linkText( "API" ) ) );
+		assertNotNull( driver.findElement( By.linkText( "Blog" ) ) );
+		assertNotNull( driver.findElement( By.className( "icon-twitter" ) ) );
+		assertNotNull( driver.findElement( By.className( "icon-facebook" ) ) );
+		
+		// check for main content
+		assertNotNull( driver.findElement( By.id( "main_content" ) ) );
+		// check for main content header
+		assertNotNull( driver.findElement( By.className( "page_header" ) ) );
+		// check for main content 
+		assertNotNull( driver.findElement( By.id( "main_column" ) ) );
+	}
 }
