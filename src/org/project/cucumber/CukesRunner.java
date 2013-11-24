c84e5fda6af5bc741d0c8c4ff822546f94b07047
Ryan Liu <ryan_lzh@hotmail.com>
2013-11-24 13:31:39 -0800

    added CukesTests class

--- src/org/project/cucumber/CukesRunner.java
+++ src/org/project/cucumber/CukesRunner.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
 
+@SuppressWarnings("deprecation")
 @RunWith(Cucumber.class)
 @Cucumber.Options(
 		format = {"pretty", "html:target/cucumber"},
-		features = "org/project/cucumber"
+		features = "src/org/project/cucumber"
 		)
 public class CukesRunner {
 
---
+++ src/org/project/cucumber/CukesTests.java
@@ -0,0 +1,5 @@
+package org.project.cucumber;
+
+public class CukesTests {
+
+}
