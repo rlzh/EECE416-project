72541528426156323c4e14b375d8a3a73844565e
Weing Kai Wee <wngkai.91@gmail.com>
2013-11-24 16:33:36 -0800

    Added JScover

---
+++ src/org/project/jscover/CoverageTest.java
@@ -0,0 +1,15 @@
+package org.project.jscover;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class CoverageTest {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
